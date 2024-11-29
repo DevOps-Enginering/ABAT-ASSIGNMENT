ce50e1cf46922f63d2a21d77d8dfe1309ac1255f
ganzamick <ganzamick@gmail.com>
2024-11-29 15:54:41 +0200

    chore: updating main.tf for ns

--- Terraform/main.tf
+++ Terraform/main.tf
@@ -1,12 +1,29 @@
 resource "null_resource" "minikube_cluster" {
   provisioner "local-exec" {
-    command = "minikube start --vm=true --driver=docker"
+    command = "minikube start"
+  }
+}
+
+resource "null_resource" "minikube_addons" {
+  depends_on = [null_resource.minikube_cluster]
+  provisioner "local-exec" {
+    command = <<-EOT
+      minikube addons enable ingress
+      minikube addons enable ingress-dns
+    EOT
   }
 }
 
 resource "kubernetes_namespace" "dev" {
-  depends_on = [null_resource.minikube_cluster]
+  depends_on = [null_resource.minikube_addons]
   metadata {
-    name = "dev-namespace"
+    name = "dev"
+  }
+}
+
+resource "kubernetes_namespace" "prod" {
+  depends_on = [null_resource.minikube_addons]
+  metadata {
+    name = "prod"
   }
 }
